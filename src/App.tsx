192734e24573606ba3649366ad63b1af955b1e64
kyrisss <kornev.roma62@gmail.com>
2022-04-30 01:32:21 +0300

    add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UsersContainer from './components/UsersPage/UsersContainer';
+import Aside from './components/Aside/Aside';
+import ProfileContainer from './components/ProfilePage/ProfileContainer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main className='main'>
+        <BrowserRouter>
+          <Aside/>
+          <Routes>
+            <Route path='/' element={<UsersContainer />} />
+            <Route path='/profile/:userId' element={<ProfileContainer />} />
+          </Routes>
+        </BrowserRouter>
+        </main>
   );
 }
 
